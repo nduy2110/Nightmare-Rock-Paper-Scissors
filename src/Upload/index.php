46f88f82a7bcf85eb4dcc9849ceadb62a753bfa9
mekanican <boommerang73@gmail.com>
2022-10-28 12:20:01 +0700

    add protection on upload

--- src/Upload/index.php
+++ src/Upload/index.php
@@ -11,17 +11,48 @@ if (!isset($_SESSION['name'])) {
     die();
 }
 
-// TODO: Adding more protection
+// Adding more protection
+// CVE-2020–35498 : https://blog.wpsec.com/contact-form-7-vulnerability/
+function change_name($filename) {
+    $parts = explode('.', $filename);
+
+    if (count($parts) < 2) {
+        return $filename;
+    }
+
+    $script_pattern = '/^(php|phtml|phar|pl|py|rb|cgi|asp|aspx)\d?$/i';
+
+    $filename = array_shift($parts);
+    $ext = array_pop($parts);
+
+    foreach( (array) $parts as $part ) {
+        if (preg_match($script_pattern, $part)) {
+            $filename .= '.' . $part . '_'; // <- mitigation
+        } else {
+            $filename .= '.' . $part;
+        }
+    }
+    if (preg_match($script_pattern, $ext)) {
+        $filename .= '.' . $ext . '_.txt'; // <- mitigation
+    } else {
+        $filename .= '.' . $ext;
+    }
+
+    return $filename;
+}
+
 if(isset($_FILES["file"])) {
     $error = '';
     $success = '';
     try {
         $filename = $_FILES["file"]["name"];
+        $filename = change_name($filename);
         $dir = $_SESSION['dir'];
         print_r($_SESSION); # DEBUG
         $file = $dir . "/" . $filename;
         move_uploaded_file($_FILES["file"]["tmp_name"], $file);
         $relative_path = substr($file, strlen($_SESSION['root'])); // Extract only relative
+        # Todo: add $file to database !
         $success = 'Successfully uploaded file at: <a href="' . $relative_path . '">' . $relative_path . ' </a><br>';
     } catch(Exception $e) {
         $error = $e->getMessage();
